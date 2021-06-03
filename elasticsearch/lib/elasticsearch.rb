3537476ce1e69152e489c55a1353d45318e52735
Fernando Briano <fernando@picandocodigo.net>
2021-06-03 15:26:14 +0100

    [CLIENT] Validates using Elasticsearch

--- elasticsearch/lib/elasticsearch.rb
+++ elasticsearch/lib/elasticsearch.rb
@@ -15,18 +15,77 @@
 # specific language governing permissions and limitations
 # under the License.
 
-require "elasticsearch/version"
-
+require 'elasticsearch/version'
 require 'elasticsearch/transport'
 require 'elasticsearch/api'
 
 module Elasticsearch
-  module Transport
-    class Client
-      include Elasticsearch::API
+  class Client
+    include Elasticsearch::API
+    SECURITY_PRIVILEGES_VALIDATION_WARNING = 'The client is unable to verify that the server is Elasticsearch due to security privileges on the server side.'.freeze
+    YOU_KNOW_FOR_SEARCH = 'You know, for Search'.freeze
+
+    def initialize(arguments = {}, &block)
+      @verified = false
+      @transport = Elasticsearch::Transport::Client.new(arguments, &block)
+    end
+
+    def method_missing(name, *args, &block)
+      verify_elasticsearch unless @verified
+      if methods.include?(name)
+        super
+      elsif @verified
+        @transport.send(name, *args, &block)
+      else
+        raise Elasticsearch::NotElasticsearchError
+      end
+    end
+
+    def verify_elasticsearch
+      begin
+        response = @transport.perform_request('GET', '/')
+      rescue Elasticsearch::Transport::Transport::Errors::Unauthorized,
+             Elasticsearch::Transport::Transport::Errors::Forbidden
+        @verified = true
+        warn(SECURITY_PRIVILEGES_VALIDATION_WARNING)
+        return
+      end
+
+      verify_with_version_or_header(response)
+    end
+
+    def verify_with_version_or_header(response)
+      version = response.body.dig('version', 'number')
+      raise Elasticsearch::NotElasticsearchError if version.nil? || version < '6.0.0'
+
+      if version == '7.x-SNAPSHOT' || Gem::Version.new(version) >= Gem::Version.new('7.14-SNAPSHOT')
+        raise Elasticsearch::NotElasticsearchError unless response.headers['x-elastic-product'] == 'Elasticsearch'
+
+        @verified = true
+      elsif Gem::Version.new(version) > Gem::Version.new('6.0.0') &&
+         Gem::Version.new(version) < Gem::Version.new('7.0.0')
+        raise Elasticsearch::NotElasticsearchError unless
+          response.body.dig('version', 'tagline') == YOU_KNOW_FOR_SEARCH
+
+        @verified = true
+      elsif Gem::Version.new(version) >= Gem::Version.new('7.0.0') &&
+            Gem::Version.new(version) < Gem::Version.new('7.14-SNAPSHOT')
+        raise Elasticsearch::NotElasticsearchError unless
+          response.body.dig('version', 'tagline') == YOU_KNOW_FOR_SEARCH &&
+          response.body.dig('version', 'build_flavor') == 'default'
+
+        @verified = true
+      end
+    end
+  end
+
+  class NotElasticsearchError < StandardError
+    def initialize
+      super('The client noticed that the server is not Elasticsearch and we do not support this unknown product.')
     end
   end
 end
+
 module Elastic
   # If the version is X.X.X.pre/alpha/beta, use X.X.Xp for the meta-header:
   def self.client_meta_version
